e4eb4cab59cc97460f5ec15ea576ed02a5b19aa3
zxsagexz <bot.error@Yandex.ru>
2017-10-14 21:23:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+  if (message.content === '!picture') {
+    message.reply(message.author.avatarURL);
+  }
+});
+
+client.on('ready', () => {
+client.user.setGame('ʕ•́ᴥ•̀ʔ', 'https://twitch.tv/antimamba777');
+});
+
+client.on('message', function(message) {
+    if (message.content == "!clear") {
+        if (message.member.hasPermission("MANAGE_MESSAGES")) {
+            message.channel.fetchMessages()
+               .then(function(list){
+                    message.channel.bulkDelete(list);
+                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})                        
+        }
+    }
+
 });
 
 // THIS  MUST  BE  THIS  WAY
