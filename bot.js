9bb057b0876ccdd596b6b1060cc4477cc7c71890
zxsagexz <bot.error@Yandex.ru>
2018-01-01 14:08:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log('I am ready!');
 });
-let prefix = "=";
+let prefix = "s";
 client.on('message', message => {
   ////////////////////////////////////////////////////////////////////////////////////////////////
   /*                        purge                                       */
